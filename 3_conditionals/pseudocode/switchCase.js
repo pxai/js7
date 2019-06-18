7c8e511dec65daacc5410c04cef0f3d4c48ec548
Pello Altadill <pello.altadill@gluckgames.com>
2019-06-18 23:02:03 +0200

    added switch sample

---
+++ 3_conditionals/pseudocode/switchCase.js
@@ -0,0 +1,13 @@
+switch (variable) {
+    case valor1:
+        operaciones;
+        break;
+    case valor2:
+        operaciones;
+        break;
+    case valor3:
+        operaciones;
+        break;
+    default:
+        operaciones;
+}
