879fb8a89d7c91f7c55cf399f61d3ce7874bbd5f
Pello Altadill <pello.altadill@gluckgames.com>
2019-06-23 20:00:54 +0200

    Added sample

---
+++ 5_arrays/propuestos/1.js
@@ -0,0 +1,10 @@
+const nombres = ["Frodo", "Merrin", "Sam", "Pip", "Bilbo"];
+
+for (let i = 0; i < nombres.length; i++) {
+    console.log(nombres[i]);
+}
+
+// variante:
+nombres.forEach( nombres => { 
+    console.log(nombre); 
+});
