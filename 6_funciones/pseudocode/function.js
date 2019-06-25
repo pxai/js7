f719597d5af63f2cd8acb1a302a5c7eba6a9a9c9
Pello Altadill <pello.altadill@gluckgames.com>
2019-06-25 23:09:20 +0200

    added functions sample

---
+++ 6_funciones/pseudocode/function.js
@@ -0,0 +1,19 @@
+function nombre () {
+    operaciones;
+}
+
+function nombre (parámetros) {
+    operaciones;
+}
+
+function nombre (parámetros) {
+    operaciones;
+
+    return resultado;
+}
+
+nombre();
+
+nombre(parámetros);
+
+let valorRetornado = nombre(parámetros);
