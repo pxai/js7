7167043b11209bfdb278dea070e4c09aa5e38f38
Pello Altadill <pello.altadill@gluckgames.com>
2019-07-01 22:38:59 +0200

    Added 4

---
+++ 1_intro/propuestos/4.js
@@ -0,0 +1,7 @@
+const nombre = prompt("Introduce tu nombre");
+
+const apellido = prompt("Introduce tu apellido");
+
+console.log(nombre + " " + apellido);
+// alternativa
+console.log(`${nombre} ${apellido}`);
