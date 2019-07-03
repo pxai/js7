5127942d0d10048f9b463cb08765fc2a0ee830d5
Pello Altadill <pello.altadill@gluckgames.com>
2019-07-03 22:39:57 +0200

    Added 4

---
+++ 1_intro/propuestos/6.js
@@ -0,0 +1,9 @@
+const booleano1 = prompt("Introduzca true o false");
+
+const booleano2 = prompt("Introduzca true o false");
+
+const and = booleano1 && booleano2;
+console.log("Resultado and: " + and);
+
+const or = booleano1 || booleano2;
+console.log("Resultado or: " + or);
