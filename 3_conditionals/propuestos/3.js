8a771bde1f56d3d2e798f9e99687ffac86b5def1
Pello Altadill <pello.altadill@gluckgames.com>
2019-07-09 22:42:35 +0200

    Added 3.js

---
+++ 3_conditionals/propuestos/3.js
@@ -0,0 +1,10 @@
+let numero = prompt("Introduce un número");
+
+if (numero >= 0) {
+ console.log(numero + " es positivo");
+} else {
+ console.log(numero + " es negativo");
+} 
+  numero = -numero;
+
+  console.log("Conversión: " + numero);
