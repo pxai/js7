027298210667dd30cbaa153e95ce23e5cd895dc9
Pello Altadill <pello.altadill@gluckgames.com>
2019-07-22 22:50:17 +0200

    Added 7.js

---
+++ 4_iteraciones/propuestos/7.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++) {
+      console.log(`${i} x ${j} = ${i * j}`);
+    }
+  }
+  
+  for (let i = 0; i <= 10; i++) {
+    for (let j = 0; j <= 10; j++) {
+      console.log(i + " x " + j + " = " + (i*j));
+    }
+  }
