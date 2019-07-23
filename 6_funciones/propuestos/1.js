455d07cf25cf2302c611c6ea24121cacbe742680
Pello Altadill <pello.altadill@gluckgames.com>
2019-07-23 20:05:34 +0200

    Added sample

---
+++ 6_funciones/propuestos/1.js
@@ -0,0 +1,18 @@
+function saludo (momentoDelDia) {
+    switch (momentoDelDia) {
+        case "mañana": return "Buenos días";
+        case "tarde": return "Buenas tardes";
+        case "noche": return "Buenas noches";
+        default: break;
+    }
+}
+
+console.log(momentoDelDia("tarde"))
+
+const momentoDelDia2 = momento => ({ 
+    "mañana": "Buenos Días", 
+    "tarde":  "Buenas tardes", 
+    "noche": "Buenas noches"}[momento]);
+    
+    
+console.log(momentoDelDia2("tarde"))
