4e11438727fea59a3e52d5542c5e625e08d012a9
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-04 09:10:08 +0200

    Added sample for js

---
+++ 1_intro/practicar/01.js
@@ -0,0 +1,6 @@
+const nombre = prompt("Introduce tu nombre");
+console.log("Hola, qué tal estás", nombre);
+
+nombre = "Juan";
+let edad = 34;
+console.log("Tu nombre es %s, tienes %d años", nombre, edad);
