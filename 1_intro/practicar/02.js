2f4c7818d74fdfb54a070e728a1ad7a752122bb9
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-05 09:10:19 +0200

    Added sample for js

---
+++ 1_intro/practicar/02.js
@@ -0,0 +1,11 @@
+const nombre = "Pello";
+const edad = 42;
+const peso = 101.54;
+const vivo = true;
+const riquezas = null;
+
+console.log(nombre);
+console.log(edad);
+console.log(peso);
+console.log(vivo);
+console.log(riquezas);
