782ec0d440852641b69f0e5e8db1877fbb6c9a56
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-07 11:27:24 +0200

    Added sample

---
+++ 3_conditionals/practicar/19.js
@@ -0,0 +1,9 @@
+const texto = prompt("Introduce un texto");
+
+if (texto === "mañana") {
+    console.log("Buenos días");
+} else if (texto === "tarde") {
+    console.log("Buenas tardes");
+} else {
+    console.log("Buenas noches");
+}
