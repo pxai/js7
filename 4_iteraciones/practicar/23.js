5ec2d7237c2e22c71f50fe63b1c42fb16fe98281
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-11 11:26:35 +0200

    Added sample

---
+++ 4_iteraciones/practicar/23.js
@@ -0,0 +1,9 @@
+//const valor = prompt("Introduce un número");
+const valor = 4;
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`${valor} x ${i} = ${valor*i}`);
+    // Alternativas: 
+    // console.log(valor + " x " + i + " = " + (valor*i));
+    // console.log("%d x %d = %d", valor, i, (valor*i));
+}
