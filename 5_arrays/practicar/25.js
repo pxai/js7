4ce7821b1d642d71d6af27d3924052f477d99e41
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-13 11:26:03 +0200

    Added sample

---
+++ 5_arrays/practicar/25.js
@@ -0,0 +1,8 @@
+const nombres = ["Frodo", "Sam", "Merrin", "Pippin"];
+const booleanos = Array(5);
+const numeros = new Array(5).fill(0);
+booleanos.fill(false);
+
+console.log(nombres);
+console.log(numeros);
+console.log(booleanos);
