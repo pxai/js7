c20ad197eda18ae3d10168e4f22168ca63a69509
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-17 09:12:53 +0200

    Added sample for js

---
+++ 1_intro/practicar/15.js
@@ -0,0 +1,6 @@
+const valor1 = prompt("Introduce un número");
+const valor2 = prompt("Introduce otro número");
+
+const resultado = (valor1 >= 0) || (valor2 >= 0);
+
+console.log("Es alguno de los dos positivo?", resultado);
