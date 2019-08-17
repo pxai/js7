1551fed432055ce0ca4d31c68a1175cd7548d71a
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-17 11:25:08 +0200

    Added sample

---
+++ 5_arrays/practicar/29.js
@@ -0,0 +1,7 @@
+const nombre = prompt("Escribe tu nombre");
+const lugar = prompt("Escribe tu lugar de nacimiento");
+const fecha = prompt("Escribe tu año de nacimiento");
+
+const mensaje = `Tu nombre es ${nombre}
+    naciste en ${lugar}
+    en el año ${fecha}`;
