3012ba952ffef36209c57b611bdc3c5cd1a7a7f1
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-19 23:23:32 +0200

    Added samples

---
+++ 07_librerias/practicar/44.js
@@ -0,0 +1,4 @@
+const mostrar = require("./44.lib");
+const datos = [3, 4, 12, 5];
+
+mostrar(datos);
---
+++ 07_librerias/practicar/44.lib.js
@@ -0,0 +1,7 @@
+function mostrar (array) {
+    for (let i = 0; i < array.length;i++){
+        console.log(array[i]);
+    }
+}
+
+module.exports = mostrar;
