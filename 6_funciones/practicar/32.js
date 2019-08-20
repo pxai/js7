01ac2ff3cf269695dcf7ea664153fd971c8ed5c1
Pello Altadill <pello.altadill@gluckgames.com>
2019-08-20 11:24:35 +0200

    Added sample

---
+++ 6_funciones/practicar/32.js
@@ -0,0 +1,15 @@
+function dias () {
+    console.log("Buenos días");
+}
+
+function tardes () {
+    console.log("Buenos tardes");
+}
+
+function noches () {
+    console.log("Buenos noches");
+}
+
+dias();
+tardes();
+noches();
