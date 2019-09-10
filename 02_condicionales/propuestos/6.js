bd819bf1ede3da7fa1d43d0ec2fd191e0f55ae38
Pello Altadill <pello.altadill@gluckgames.com>
2019-09-10 21:47:24 +0200

    corrected sample files

--- 02_condicionales/propuestos/6.js
+++ 02_condicionales/propuestos/6.js
@@ -1,23 +1,27 @@
 const dorsal = prompt("Introduce dorsal");
 
-switch (parseInt(dorsal)) {
-  case 1:
-        console.log("Portero");
-        break;
-  case 3:
-  case 4:
-  case 5:
-        console.log("Defensa");
-        break;
-  case 6:
-  case 8:
-  case 11:
-    console.log("Centrocampista");
-        break;
-  case 9:
-        console.log("Delantero");
-        break;
-  default:
-        console.log("Cualquiera");
-        break;
+if (dorsal >= 0 && dorsal <= 99) {
+      switch (parseInt(dorsal)) {
+            case 1:
+                  console.log("Portero");
+                  break;
+            case 3:
+            case 4:
+            case 5:
+                  console.log("Defensa");
+                  break;
+            case 6:
+            case 8:
+            case 11:
+              console.log("Centrocampista");
+                  break;
+            case 9:
+                  console.log("Delantero");
+                  break;
+            default:
+                  console.log("Cualquiera");
+                  break;
+          }
+} else {
+      console.log("Error, el dorsal no está entre 0 y 99");
 }
